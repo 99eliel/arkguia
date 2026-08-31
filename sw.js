f0129f3266583d7c501349688405ad6d0be0f76f
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-30 23:05:30 -0300

    feat: cache crafting planner modules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='arkguia-v2.6.3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./creatures.js','./services.js','./resource-data.js','./progress-migration.js','./firebase-sync.js','./app.js','./creature-images.js'];
+const CACHE='arkguia-v2.7.0';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./creatures.js','./services.js','./resource-data.js','./progress-migration.js','./firebase-sync.js','./app.js','./creature-images.js','./crafting-data.js','./crafting.js'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
