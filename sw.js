29fd2d5b9b5b37780b9ce0e2fe8b014565da93ec
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-30 22:47:13 -0300

    Bump PWA cache for Firebase-primary resource update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='arkguia-v2.4.0';
+const CACHE='arkguia-v2.5.0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./creatures.js','./services.js','./resource-data.js','./progress-migration.js','./firebase-sync.js','./app.js'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
